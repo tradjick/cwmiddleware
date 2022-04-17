f564184be2ecff861e1eb589fe79c47a8c506a87
tradjick <tradjick@gmail.com>
2022-04-17 15:30:26 +1000

    remove logging from requestid

--- cwmwadapters/embedrequestid.go
+++ cwmwadapters/embedrequestid.go
@@ -3,18 +3,16 @@ package cwmwadapters
 import (
 	"context"
 	cwmiddlware "github.com/tradjick/cwmiddleware"
-	"log"
 	"net/http"
 )
 
 type RequestIdGenerator func() ([]byte, error)
 
-func EmbedRequestId(g RequestIdGenerator, l *log.Logger) cwmiddlware.Adapter {
+func EmbedRequestId(g RequestIdGenerator) cwmiddlware.Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			reqId, err := g()
 			if err != nil {
-				l.Printf("error creating http request id : %s", err)
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				return
 			}
